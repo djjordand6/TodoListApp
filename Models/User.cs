f35e6b3044b52595d6f2b825498a1998376c4a41
Jordan D <jordan.dsouza@ntlworld.com>
2023-09-13 10:38:05 +0100

    Name field added

--- Models/User.cs
+++ Models/User.cs
@@ -7,5 +7,7 @@
         public string Email { get; set; } = string.Empty;
 
         public string Pass { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
     }
 }
