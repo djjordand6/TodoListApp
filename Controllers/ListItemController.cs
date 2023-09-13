e4f8a4f9be4cd2a5729c25fa92c85618ac73fd58
Jordan D <jordan.dsouza@ntlworld.com>
2023-09-13 09:48:02 +0100

    ListItemController.cs creation

---
+++ Controllers/ListItemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoListApp.Models;
+
+namespace TodoListApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListItemController : ControllerBase
+    {
+    }
+}
