5d86d9948f830955bcd235122acd1e41b4dbe7e4
Jordan D <jordan.dsouza@ntlworld.com>
2023-09-13 09:48:53 +0100

    UserController.cs creation w/ some methods

---
+++ Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using TodoListApp.Models;
+
+namespace TodoListApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // Temp Variable to test methods
+        private static List<User> users = new List<User>
+        {
+            new User { Id = 1, Email = "test@test.com", Pass = "hello123" }, //No Hashing
+            new User { Id = 2, Email = "test2@testing.com", Pass = "goodbye456" }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetAllUsers()
+        {
+            return Ok(users);
+        }
+
+        [HttpGet("{email} {pass}")]
+        public async Task<ActionResult<User>> GetUser(String email, String pass)
+        {
+            var user = users.Find(user => user.Email == email && user.Pass == pass);
+
+            if(user is null)
+                return NotFound("User not found");
+
+            return Ok(user); //Should return user ID so it can be used to return all ListItem's with the corresponding UserID field
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> AddUser([FromBody] User u)
+        {
+            users.Add(u);
+            return Ok(users);
+        }
+    }
+}
