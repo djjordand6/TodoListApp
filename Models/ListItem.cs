85fdcdb239e1e249e54647239a0e392adfb99c63
Jordan D <jordan.dsouza@ntlworld.com>
2023-09-13 09:47:30 +0100

    ListItem.cs + User.cs model creation

---
+++ Models/ListItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoListApp.Models
+{
+    public class ListItem
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public string ItemText { get; set; } = string.Empty;
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoListApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Pass { get; set; } = string.Empty;
+    }
+}
