044eb68be19e712a5b0c14eceb8486750b23167e
Jordan D <jordan.dsouza@ntlworld.com>
2023-09-15 11:04:00 +0100

    DataContext created

---
+++ Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace TodoListApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=TodoListDB;Trusted_Connection=true;TrustServerCertificate=true;");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<ListItem> ListItems { get; set; }
+    }
+}
