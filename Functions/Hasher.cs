9382436cb600146c82e68fdc9f8b2c5fc53754a7
Jordan D <jordan.dsouza@ntlworld.com>
2023-09-15 11:25:05 +0100

    Hasher.cs created (for password hashing)

---
+++ Functions/Hasher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TodoListApp.Functions
+{
+    public class Hasher
+    {
+        private SHA256 ha = SHA256.Create();
+
+        public string GetHash(string s)
+        {
+            byte[] data = ha.ComputeHash(Encoding.UTF8.GetBytes(s));
+
+            var sb = new StringBuilder();
+
+            for(int i = 0; i < data.Length; i++)
+            {
+                sb.Append(data[i].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+
+        public bool VerifyHash(string s, string hash)
+        {
+            var inputHash = GetHash(s);
+
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            return comparer.Compare(inputHash, hash) == 0;
+        }
+    }
+}
