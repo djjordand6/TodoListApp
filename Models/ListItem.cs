13e6cefe3a19ff1f20b980097686e7f6e3607bab
Jordan D <jordan.dsouza@ntlworld.com>
2023-09-25 13:00:29 +0100

    Removed unnecessary required keywords

--- Models/ListItem.cs
+++ Models/ListItem.cs
@@ -4,14 +4,14 @@
     {
         public int Id { get; set; }
 
-        public required int UserId { get; set; }
+        public int UserId { get; set; }
 
         public required string ItemText { get; set; } = string.Empty;
 
-        public required bool IsDone { get; set; } = false;
+        public bool IsDone { get; set; } = false;
 
         public int Order {  get; set; }
 
-        public required DateTime Created { get; set; }
+        public DateTime Created { get; set; }
     }
 }
