c75c21999fa9d0977c06f9f9bb8cd2fe1f3558c8
Jordan D <jordan.dsouza@ntlworld.com>
2023-09-25 12:29:55 +0100

    Removal of GetAllTodo function from interface

--- Services/ListItemService/IListItemService.cs
+++ Services/ListItemService/IListItemService.cs
@@ -4,7 +4,7 @@ namespace TodoListApp.Services.ListItemService
 {
     public interface IListItemService
     {
-        Task<List<ListItem>> GetAllTodo();
+        // Task<List<ListItem>> GetAllTodo();
         Task<List<ListItem>?> GetUserTodo(int userId);
         Task<List<ListItem>> AddTodo(ListItem li);
         Task<List<ListItem>?> EditTodo(int id, ListItem req);
