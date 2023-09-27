c72df9d0c323bfe068b4102d6fb8b6d7e353765e
Jordan D <jordan.dsouza@ntlworld.com>
2023-09-27 09:42:44 +0100

    Comment denoting actual relational fields

--- Models/ListItem.cs
+++ Models/ListItem.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
 
-        public int UserId { get; set; }
+        public int UserId { get; set; } //public virtual User User { get; set; }
 
         public required string ItemText { get; set; } = string.Empty;
 
